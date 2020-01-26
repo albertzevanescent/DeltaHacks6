71f5a0fce6bdd8f3aca182fcfe81a68b20fa921a
Gilbert Cherrie <gilbert.cherrie@outlook.com>
2020-01-25 22:55:13 -0500

    Edit

--- write_page.php
+++ write_page.php
@@ -1,12 +1,24 @@
 <?php
-$name = $_POST("name");
-$description = $_POST("description");
-$promotion = $_POST("promotions");
-$lat = $_POST("lat");
-$long = $_POST("long");
-$vacancy = $_POST("vacancy");
-$sql = "INSERT INTO table_name (column1, column2, column3,...)
-VALUES ('name', 'description', 'promotions,'lat','long','vacancy')";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stores";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST["name"];
+$description = $_POST["description"];
+$promotion = $_POST["promotions"];
+$lat = $_POST["lat"];
+$long = $_POST["long"];
+$vacancy = $_POST["vacancy"];
+$sql = "INSERT INTO stores (Name, Description, Promotions, LatLoc, LongLoc, Vacancy)
+VALUES ('$name', '$description', '$promotion','$lat','$long','$vacancy')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
